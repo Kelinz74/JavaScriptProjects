fabdf5aee45d434006ee52a9ab188564af1a4a34
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-08-09 12:58:21 -0700

    Basic JavaScript Projects Submission Assignment
    Utilized the window.alert() and document.write() methods, Assigned a string value to two variables and concatenated the variables together, Assign a color to at least on variable using the fontcolor method, wrote an expression, and commented through out the JavaScript file

---
+++ BasicJavaScriptProjects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,39 @@
+//Demonstrating a alert JavaScript
+
+var A = "\"What do you mean?, "
++ "Do you wish me a good morning, or mean that it is a good morning whether I want it or not;"
++ " or that you feel good this morning; or that it is a morning to be good on?\"";
+
+//Concatenating two Strings in JavaScript and changed the font color
+
+var B = " Concatenated"
+var C = " String"
+var B = B.fontcolor("green")
+
+//Made a Multi Variable and changed the font color.
+
+var Avengers = " Protecters of the world.", Leader = " Captain America ", Money = " Iron Man", 
+Strength = " Incredible Hulk";
+var Leader = Leader.fontcolor("red")
+
+//Changed the font color.
+
+var blues = "I have the blues. ";
+var blues = blues.fontcolor("blue");
+
+//font color print
+document.write(blues);
+
+//an alert
+alert(A)
+
+//var B + var C
+document.write(B + C)
+
+//var Leader
+document.write(Leader)
+
+//a statment containing an expression (3 + 3)
+document.write(3+3);
+
+ 
