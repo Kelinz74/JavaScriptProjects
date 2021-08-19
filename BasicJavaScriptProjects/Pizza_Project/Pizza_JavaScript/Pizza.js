e356a8281ebe0d9c54c992ba243ff9f4fa793ec5
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-08-19 13:42:16 -0700

    Pizza Menu Vegetable Assignment
    Submitted a fully functional pizza app, added comments through the HTML, CSS and JavaScript files explaining the code.

---
+++ BasicJavaScriptProjects/Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -0,0 +1,61 @@
+function getReceipt() {
+    // This initializes our string so it can get passed from
+    // function to funtion, growing line by line into a full receipt
+    var text1 = "<h3>You Ordered:</h3>";
+    var runningTotal = 0;
+    var sizeTotal = 0;
+    var sizeArray = document.getElementsByClassName("size");
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1+selectedSize+"<br>";
+        }
+    }
+    //if statement for the size of pizza that is chosen with the cost value
+    if (selectedSize === "Personal Pizza") {
+        sizeTotal = 6;
+    } else if (selectedSize === "Small Pizza") {
+        sizeTotal = 8;
+    } else if (selectedSize === "Medium Pizza") {
+        sizeTotal = 10;
+    } else if (selectedSize === "Large Pizza") {
+        sizeTotal = 14;
+    } else if (selectedSize === "Extra Large Pizza") {
+        sizeTotal = 16;
+    }
+    runningTotal = sizeTotal;
+    console.log(selectedSize+" = $"+sizeTotal+".00");
+    console.log("size text1: "+text1);
+    console.log("subtotal: $"+runningTotal+".00");
+    //these variable will get passed on to each function
+    getTopping(runningTotal,text1);
+};
+//a function with a loop that will create a list of the toppings chosen
+function getTopping(runningTotal,text1) {
+    var toppingTotal = 0;
+    var selectedTopping = [];
+    var toppingArray = document.getElementsByClassName("toppings");
+    for (var j = 0; j < toppingArray.length; j++) {
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value);
+            console.log("selected topping item: ("+toppingArray[j].value+")");
+            text1 = text1+toppingArray[j].value+"<br>";
+        }
+    }
+    //a variable to hold the totale topping count from the function above from a 
+    //.length instance of selectedTopping list
+    var toppingCount = selectedTopping.length;
+    if (toppingCount > 1) {
+        toppingTotal = (toppingCount - 1);        
+    } else {
+        toppingTotal = 0;
+    }
+    //final total of size pizza + toppings, then displayed as a list of what was ordered with the final cost.
+    runningTotal = (runningTotal + toppingTotal);
+    console.log("total selected topping items: "+toppingCount);
+    console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
+    console.log("topping text1: "+text1);
+    console.log("Purchase Total: "+"$"+runningTotal+".00");
+    document.getElementById("showText").innerHTML=text1;
+    document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";        
+};
